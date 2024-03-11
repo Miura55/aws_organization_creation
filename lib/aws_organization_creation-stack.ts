3b1a1021c2b94908a49af8cf4341d7d3c2d94822
Miura55 <acordion.piano@gmail.com>
2024-03-12 01:36:46 +0900

    AWS組織を作成するコードを追加

--- lib/aws_organization_creation-stack.ts
+++ lib/aws_organization_creation-stack.ts
@@ -1,16 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as organizations from 'aws-cdk-lib/aws-organizations';
 
 export class AwsOrganizationCreationStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsOrganizationCreationQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create an AWS Organization
+    new organizations.CfnOrganization(this, 'MyOrganization', {
+      featureSet: 'ALL',
+    });
   }
 }
